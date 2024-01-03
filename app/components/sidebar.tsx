b28cd76cd6dac6b706d96846d10d9ab5c05e9036
jiumu <59447202+370738206@users.noreply.github.com>
2024-01-03 18:56:58 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          NextChat-JIUMU部署
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          反馈问题/其他需求联系QQ群：296313037.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
